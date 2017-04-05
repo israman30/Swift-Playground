8855639fab5484767fd304fb52544000635e700b
Israel Manzo <israman814@gmail.com>
2017-04-05 19:47:08 -0400

    updating playground

--- Conditionals&Loops&Logics.playground/Contents.swift
+++ Conditionals&Loops&Logics.playground/Contents.swift
@@ -5,6 +5,8 @@ import UIKit
 // Conditionals are logic statements
 // Condicionales son estatutos logicos
 
+// ============ if statements ==============
+
 // Example: This simple statement
 if "John" == "Johnny" {
     
@@ -35,3 +37,55 @@ if name == age {
     print("He is ready for his beer.")
     
 }
+
+
+
+
+// ============ For loop ==============
+
+for number in [1,2,3,4,5] {
+    print(number)
+}
+
+
+// We are given the array in a container
+// Ponemos el array de numeros in a container
+let numbers = [4,3,5,12,67]
+// then
+for i in numbers {
+    print(i)
+}
+
+// We are looping in an string array
+let words = ["Juan", "Carlos", "Tommy", "Sarah"]
+for person in words {
+    print(person)
+}
+
+
+// We Have an array of countries
+// Tenemos un array de paises
+let countries = ["US", "Ecuador", "Brasil", "Canada", "Argentina", "Mexico", "Colombia", "Chile", "Bolivia", "Peru", "Panama", "Venezuela"]
+// We will loop in this array an put the countries in the correct continent container
+
+var america = [String]() // Empty array - array vacia
+
+// We loop
+
+for country in countries {
+    // We append each country in the array america
+    // Aderimos cada pais en el array america
+    america.append(country)
+}
+
+// Now america contains the countries
+// Ahora america contiene los paises
+america
+
+
+
+
+
+
+
+
