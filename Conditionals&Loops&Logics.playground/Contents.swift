4b8f9bb784ac06c59aa6cf8ef95d83098903338d
Israel Manzo <israman814@gmail.com>
2017-04-05 19:25:03 -0400

    updating conditional playgorund

--- Conditionals&Loops&Logics.playground/Contents.swift
+++ Conditionals&Loops&Logics.playground/Contents.swift
@@ -2,4 +2,26 @@
 
 import UIKit
 
+// Conditionals are logic statements
+// Condicionales son estatutos logicos
 
+// Example: This simple statement
+if "John" == "Johnny" {
+    print("The are the same guy")
+} else {
+    print("The are not the same guy")
+}
+
+
+// We have this statement checking if Tom is allow to drink
+// Tenemos este estatuto si Tom tiene permitido beber
+var age = "21"
+var name = "Tom"
+
+if name == age {
+    print("Her is allow to drink")
+} else if name != age {
+    print("He is not allow to drink yet")
+} else {
+    print("He is ready for his beer.")
+}
