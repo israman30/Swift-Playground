5613722582fdb85c6a62edb4dd91a91fd6ef5ffb
Israel Manzo <israelmanzo814@gmail.com>
2018-08-30 21:50:33 -0400

    Adding animation on table view cells

--- ReusableElments.playground/Contents.swift
+++ ReusableElments.playground/Contents.swift
@@ -59,15 +59,35 @@ extension UIImageView {
  - TableView animation
  - TableView cell will animate transforming the scale x, y when viewWillAppear
  */
-extension TableViewClass {
+
     
-    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    
+    cell.layer.transform = CATransform3DMakeScale(0.1, 0.1, 1.0)
+    
+    UIView.animate(withDuration: 0.5, animations: {
+        cell.layer.transform = CATransform3DMakeScale(1.0, 1.0, 1.0)
+    }, completion: nil)
+}
+
+extension UITableView {
+    
+    func animateTableView() {
+        reloadData()
+        let cells = visibleCells
+        let tableViewEffects = bounds.size.height
         
-        cell.layer.transform = CATransform3DMakeScale(0.1, 0.1, 1.0)
+        for cell in cells {
+            cell.transform = CGAffineTransform(translationX: 0, y: tableViewEffects)
+        }
         
-        UIView.animate(withDuration: 0.5, animations: {
-            cell.layer.transform = CATransform3DMakeScale(1.0, 1.0, 1.0)
-        }, completion: nil)
+        var delay:Double = 0
+        for cell in cells {
+            UIView.animate(withDuration: 1.75, delay: delay * 0.05, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
+                cell.transform = CGAffineTransform.identity
+            }, completion: nil)
+            delay += 1
+        }
     }
 }
 
