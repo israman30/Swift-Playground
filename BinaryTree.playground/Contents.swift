fc310ec84d468271ab195ddad59cb8363eb11e37
Israel Manzo <israelmanzo814@gmail.com>
2020-01-21 18:54:51 -0500

    Added binary tree playground

---
+++ BinaryTree.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+// =========================== BINARY TREE PLAYGROUND ===========================
+// =    * Binary Tree class                                                     =
+// =    * Binary Tree constructor                                               =
+// =    1. Insert Node                                                          =
+// =    2. Search and return a boolean                                          =
+// =    3. Search and return a tree                                             =
+// =    4. Tree height                                                          =
+// =    5. Tree depth                                                           =
+// =    6. Minimun node                                                         =
+// =    7. Maximun node                                                         =
+// =    8. Delete all leaf                                                      =
+// =    9. Delete a node                                                        =
+// =    10. Inorder Traverse                                                    =
+// =    11. Preorder Traverse                                                   =
+// =    12. Postorder Traverse                                                  =
+// =    13. Lever Order Traverse                                                =
+// =    14. Array into a tree                                                   =
+// =    15. Max depth of the tree                                               =
+// =    16. Mirror the tree                                                     =
+// ==============================================================================
+
+
