53fd47c045572c9d92db6786d20fc26ac2e77064
Israel Manzo <israelmanzo814@gmail.com>
2020-05-02 11:30:57 -0400

    Added alertController component

--- ReusableElments.playground/Contents.swift
+++ ReusableElments.playground/Contents.swift
@@ -2,35 +2,7 @@
 
 import UIKit
 
-// MARK: - Alert - Action Sheet Controller
-struct AlertController {
-    
-    static func alert(viewController: UIAlertController, title: String, message: String){
-        
-        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
-        alertController.addAction(action)
-        
-        viewController.present(alertController, animated: true, completion: nil)
-    }
-    
-    static func actionSheet(viewController: UIAlertController, title: String, message: String){
-        let actionSheet = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
-        
-        let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        // TODO: add aditional buttons
-        
-        actionSheet.addAction(cancel)
-        // TODO: add aditional buttons in actionSheet
-        
-        viewController.present(actionSheet, animated: true, completion: nil)
-    }
-}
 
-/* Usage:
-    pass the Alert class and alert function
-    AlertController.alert(viewController: self,title: "My Title",message: "My message")
- */
 
 
 //=====================================================================================================
