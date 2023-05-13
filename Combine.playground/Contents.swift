523639f36bb41683e8d411fcf9a32ebb6c2e3b92
@israman30 <israelmanzo@israels-mbp-2.myfiosgateway.com>
2023-05-13 14:48:13 -0400

    updated subscriptor timer

--- Combine.playground/Contents.swift
+++ Combine.playground/Contents.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Combine
+import PlaygroundSupport
 
 /*
  ombine was introduced as a new framework by Apple at WWDC-2019. It provides a declarative Swift API for processing values over time.
@@ -25,6 +26,7 @@ import Combine
         print("Received value: \(value)")
     }
 
+
 /*
  NotificationCenter.Publisher
  Let’s create a new publisher for a new-event notification.
@@ -46,10 +48,49 @@ let eventPublisher = NotificationCenter.Publisher(center: .default, name: .added
 
 let theEventTitleLabel = UILabel()
 
-let newEventLabelSubscriber = Subscribers.Assign(object: theEventTitleLabel, keyPath: \.text)
-eventPublisher.subscribe(newEventLabelSubscriber)
+/*let newEventLabelSubscriber = Subscribers.Assign(object: theEventTitleLabel, keyPath: \.text)
+eventPublisher.subscribe(newEventLabelSubscriber) */
+
+eventPublisher.assign(to: \.text, on: theEventTitleLabel)
+/**
+ The assign(to:on) operator subscribes to the notification publisher and links to the lifetime of the label. Once the label gets released, its subscription gets released too.
+ */
 
 let event = Event(title: "Combine framework", scheduled: Date())
 NotificationCenter.default.post(name: .addedEvent, object: event)
 
 print("Event notified: \(theEventTitleLabel.text ?? "No event")")
+
+
+// MARK: - Timer Subscription, reating a Subscription
+
+let subscriptor = Timer.publish(every: 1, on: .main, in: .common)
+    .autoconnect()
+    .sink { output in
+        print("Finish stream with: \(output)")
+    } receiveValue: { value in
+        print("Receive value: \(value)")
+    }
+
+/**
+ Cancelling the Subscription
+ Call cancel()
+ */
+RunLoop.main.schedule(after: .init(Date(timeIntervalSinceNow: 4))) {
+    print(" - cancel subscription")
+    subscriptor.cancel()
+    /// or
+//    subscriptor = nil
+}
+
+/*
+ Timer.publish(): Returns a publisher that repeatedly emits the current date on the given interval.
+
+ .autoconnect: Starts the timer when the Timer object is created.
+
+ sink {}:
+
+ output block: called when the subscription is finished.
+ receiveValue block: called when any value is published
+ */
+
