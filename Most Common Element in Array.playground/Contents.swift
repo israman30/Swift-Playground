eda069533b3c8236ea53867289652d4e781b06e7
@israman30 <israelmanzo@israels-mbp-2.myfiosgateway.com>
2023-05-14 15:03:04 -0400

    added commets

--- Most Common Element in Array.playground/Contents.swift
+++ Most Common Element in Array.playground/Contents.swift
@@ -20,6 +20,7 @@ func mostCommonElement(of array: [Int]) -> Int {
 
 mostCommonElement(of: array)
 
+/// Most Common found looping in the array and checking in the dictionary for max() value
 func mostCommonElement2(of array: [Int]) -> (value: Int, count: Int) {
     var dict = [Int:Int]()
     array.forEach { dict[$0] = (dict[$0] ?? 0) + 1 }
@@ -32,6 +33,7 @@ func mostCommonElement2(of array: [Int]) -> (value: Int, count: Int) {
 }
 mostCommonElement2(of: array)
 
+/// Most Common usinge reduce in the dictionary and checking for the max() value
 func mosCommon3(of array: [Int]) -> (value: Int, count: Int) {
     let dict = array.reduce(into: [:]) { $0[$1, default: 0] += 1 }
     if let (value, count) = dict.max(by: {$0.1 < $1.1}) {
