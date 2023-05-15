b3e8d973fe61d8dfc4861cfd52cdec52556a9b5d
@israman30 <israelmanzo@israels-mbp-2.myfiosgateway.com>
2023-05-14 22:37:56 -0400

    added insert node in tree + tree count and root nil

--- BinaryTree.playground/Contents.swift
+++ BinaryTree.playground/Contents.swift
@@ -28,24 +28,57 @@ import UIKit
  
  */
 
+var array = [2, 3, 4, 5, 6, 7, 8]
+
 // Representing in memory
-class Node {
-    var leftNode: Node?
-    var rightNode: Node?
+class BinaryTree {
+    var parent: BinaryTree?
+    var leftNode: BinaryTree?
+    var rightNode: BinaryTree?
     var value: Int
     init(_ value: Int) {
         self.value = value
     }
 }
 
+extension BinaryTree {
+    
+    var isRoot: Bool {
+        parent == nil
+    }
+    
+    var treeCount: Int {
+        (leftNode?.treeCount ?? 0) + 1 + (rightNode?.treeCount ?? 0)
+    }
+    
+    func insert(value: Int) {
+        if value < self.value {
+            if let left = leftNode {
+                left.insert(value: value)
+            } else {
+                leftNode = BinaryTree(value)
+                leftNode?.parent = self
+            }
+        } else {
+            if let right = rightNode {
+                right.insert(value: value)
+            } else {
+                rightNode = BinaryTree(value)
+                rightNode?.parent = self
+            }
+        }
+    }
+}
+
+
 // contruct a binary tree from array
-func binaryTreeFrom(arr: [Int], firtIndex: Int, lastIndex: Int) -> Node? {
+func binaryTree(from arr: [Int], firtIndex: Int, lastIndex: Int) -> BinaryTree? {
     if lastIndex < firtIndex {
         return nil
     }
     let mid = (firtIndex + lastIndex) / 2
-    let node = Node(arr[mid])
-    node.leftNode = binaryTreeFrom(arr: arr, firtIndex: firtIndex, lastIndex: mid - 1)
-    node.rightNode = binaryTreeFrom(arr: arr, firtIndex: mid + 1, lastIndex: lastIndex)
+    let node = BinaryTree(arr[mid])
+    node.leftNode = binaryTree(from: arr, firtIndex: firtIndex, lastIndex: mid - 1)
+    node.rightNode = binaryTree(from: arr, firtIndex: mid + 1, lastIndex: lastIndex)
     return node
 }
