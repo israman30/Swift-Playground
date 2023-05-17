86cb860a2e2e5d2b2d2fa811de75b8e9bc3b0764
@israman30 <israelmanzo@israels-mbp-2.myfiosgateway.com>
2023-05-17 17:49:50 -0400

    added sum of two with pointer

--- Two Sum Problem.playground/Contents.swift
+++ Two Sum Problem.playground/Contents.swift
@@ -9,17 +9,33 @@ func sumOfTwo(arr: [Int], target: Int) -> (Int, Int) {
     for (index, number) in arr.enumerated() {
         let complement = target - number
         print(complement)
+        print("checking:", dict)
         // check if complemet exist in dictionary
         if let complementIndex = dict[complement] {
+            print("found:", complementIndex)
             return (complementIndex, index)
         }
         // store number and its index into dictionary
         dict[number] = index
+        print("-->", dict)
     }
     return (0,0)
 }
 
-//sumOfTwo(arr: [1, 2, 3], target: 5)
+sumOfTwo(arr: [1, 2, 3], target: 5)
+/*
+ 1st loop ...........
+ 5 - 1 = 4
+ 4 exist -> dict [:] pass
+ dict [1:0]
+ 2nd loop ...........
+ 5 - 2 = 3
+ 3exist -> dict [1:0] pass
+ dict [1:0, 2:1]
+ 3rd loop ...........
+ 5 - 3 = 2
+ 2 exits -> dict [1:0, 2:1] return
+ */
 
 func sumOfTwoWithPointers(arr: [Int], target: Int) -> (Int, Int) {
     var first = 0
@@ -36,4 +52,14 @@ func sumOfTwoWithPointers(arr: [Int], target: Int) -> (Int, Int) {
     }
     return (0, 0)
 }
-sumOfTwoWithPointers(arr: [1, 2, 3], target: 5)
+//sumOfTwoWithPointers(arr: [1, 2, 3], target: 5)
+
+/*
+ 1st loop ...........
+ 1 + 3 = 4
+ 5 != 4 pass && 4 < 5
+ 1 += 1
+ 2nd loop ...........
+ 2 + 3 = 5
+ 5 == 5 return
+ */
