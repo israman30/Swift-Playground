f0f5cc63ac31de56973af983b1c0516d367633b6
@israman30 <israelmanzo@israels-mbp-2.myfiosgateway.com>
2023-05-31 23:07:00 -0400

    added Queue and Stack struct to Graph playground

--- Graphs.playground/Contents.swift
+++ Graphs.playground/Contents.swift
@@ -46,4 +46,68 @@ class Graph<T> {
     }
 }
 
-//let graph =
+struct Queue<T> {
+    private var array: [T]
+    
+    init() {
+        self.array = []
+    }
+    
+    mutating func enqueue(_ element: T) {
+        array.append(element)
+    }
+    
+    mutating func dequeue() -> T? {
+        if array.isEmpty {
+            return nil
+        }
+        return array.removeLast()
+    }
+}
+
+struct Stack<T> {
+    private var array: [T]
+    
+    init() {
+        self.array = []
+    }
+    
+    mutating func push(_ element: T) {
+        array.append(element)
+    }
+    
+    mutating func isEmpty() -> Bool {
+        array.isEmpty
+    }
+    
+    mutating func pop() -> T? {
+        guard !isEmpty() else { return nil }
+        return array.removeFirst()
+    }
+    
+}
+
+let graph = Graph<Int>()
+
+// Let's add vertex for the graph
+let node1 = graph.addVertex(1)
+let node2 = graph.addVertex(2)
+let node3 = graph.addVertex(3)
+let node4 = graph.addVertex(4)
+let node5 = graph.addVertex(5)
+let node6 = graph.addVertex(6)
+let node7 = graph.addVertex(7)
+let node8 = graph.addVertex(8)
+let node9 = graph.addVertex(9)
+let node10 = graph.addVertex(10)
+
+// Let's add edges
+graph.addEdge(node1, neighbour: node2)
+graph.addEdge(node1, neighbour: node3)
+graph.addEdge(node1, neighbour: node4)
+graph.addEdge(node1, neighbour: node5)
+graph.addEdge(node2, neighbour: node6)
+graph.addEdge(node3, neighbour: node6)
+graph.addEdge(node3, neighbour: node7)
+graph.addEdge(node4, neighbour: node7)
+graph.addEdge(node5, neighbour: node8)
