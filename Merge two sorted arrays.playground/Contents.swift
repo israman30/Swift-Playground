8cabdd42f3968eb851503abbb62e1524adf3ec08
Israel Manzo <israelmanzo814@gmail.com>
2025-03-17 15:27:15 -0400

    added playground for merging two sorted arrays

---
+++ Merge two sorted arrays.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+func mergeTwoSortedArray(_ firstArray: [Int], _ secondArray: [Int]) -> [Int] {
+    var meregedArray = [Int]()
+    var firstIndex = 0
+    var secondIndex = 0
+    while firstIndex < firstArray.count && secondIndex < secondArray.count {
+        if firstArray[firstIndex] < secondArray[secondIndex] {
+            meregedArray.append(firstArray[firstIndex])
+            firstIndex += 1
+        } else {
+            meregedArray.append(secondArray[secondIndex])
+            secondIndex += 1
+        }
+    }
+    
+    while firstIndex < firstArray.count {
+        meregedArray.append(firstArray[firstIndex])
+        firstIndex += 1
+    }
+    
+    while secondIndex < secondArray.count {
+        meregedArray.append(secondArray[secondIndex])
+        secondIndex += 1
+    }
+    
+    return meregedArray
+}
