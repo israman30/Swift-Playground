8b56ea2b34ef500b174f9d256468b2b4ac6f4e9d
Israel Manzo <israelmanzo814@gmail.com>
2025-08-17 00:17:11 -0400

    updated function

---
+++ Sandbox.playground/Pages/Reversed.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var array: [Int] = [1, 2, 3, 4, 5]
+
+func reverse<T>(_ array: inout [T]) -> [T] {
+    for element in 0..<array.count / 2 {
+        array.swapAt(element, array.count - 1 - element)
+    }
+    return array
+}
+
+reverse(&array)
