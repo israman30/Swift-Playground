74ae0a6b5a911b8abbf373d2c406c0733298b376
Israel Manzo <israelmanzo814@gmail.com>
2025-08-19 22:44:36 -0400

    adding map

--- Sandbox.playground/Pages/Functions.xcplaygroundpage/Contents.swift
+++ Sandbox.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -30,3 +30,17 @@ let sorteNumbers = numbers.sorted()
 let sortedByNumbers = numbers.sorted(by: <)
 print(sorteNumbers)
 print("Sorted by: \(sortedByNumbers)")
+
+/**
+ `Map
+ Mapping is similar to sort in that it iterates through the array that is calling it, but instead of sorting it changes each element of the array based on the closure passed to the method.
+ 
+ ```
+ let element = numbers.map( transform: (Int) throws -> T)
+ ```
+ Notice that the return of the closure is a T. Since an array is a generic type and we are returning a new array, we can return an array with a type different than the starting array (if we so choose). Lets convert our numbers into strings.
+ */
+let numbersAsString = numbers.map { char in
+    String(char)
+}
+print("Mapping to string: \(numbersAsString)")
