57ca71434a225700ffd16a382861d332e6b88e8d
Israel Manzo <israelmanzo814@gmail.com>
2025-08-29 22:23:45 -0400

    added quick sort using filter

--- Sandbox.playground/Pages/Functions.xcplaygroundpage/Contents.swift
+++ Sandbox.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -149,3 +149,17 @@ func reverseString(_ input: inout String) -> String {
     return input.map(String.init).joined()
 }
 reverseString(&word)
+
+func quickSort(_ array: [Int]) -> [Int] {
+    guard array.count > 1 else {
+        return array
+    }
+    
+    let pivot = array[array.count/2]
+    let less = array.filter { $0 < pivot }
+    let equal = array.filter { $0 == pivot }
+    let greater = array.filter { $0 > pivot }
+    
+    return quickSort(less) + equal + quickSort(greater)
+}
+quickSort(numbers)
