58cd3e86d62ba74317c8a14905772a7705258231
Israel Manzo <israelmanzo814@gmail.com>
2025-08-30 22:30:10 -0400

    added new playground

---
+++ Sandbox.playground/Pages/Data Structures.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// String
+var stringWord = "This is an string"
+
+// Integer
+var integerNumber: Int = 10
+
+// Float
+var floatNumber: Float = 10.5
+
+// Double
+var doubleNumber: Double = 10.55
+
+// Boolean
+var booleanValue: Bool = true
