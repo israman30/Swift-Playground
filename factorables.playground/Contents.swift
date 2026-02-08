fedfc081f7c0209688c17359b42b52b0d0097cdd
Israel Manzo <israelmanzo814@gmail.com>
2026-02-07 22:35:38 -0500

    added new playground with factorables

---
+++ factorables.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+// factorial
+func factorial(_ n: Int) -> Int {
+    return (1...n).reduce(1, *)
+}
+
+// Recursive factorial
+func factorialRecursive(_ n: Int) -> Int {
+    if n == 0 { return 0 }
+    return n * factorialRecursive(n - 1)
+}
+
+// factorable numbers
+/*
+ 1 -> 1
+ 3 -> 1, 2
+ 4 -> 1, 2, 4
+ 5 -> 1, 5
+ 6 -> 1, 2, 3, 6
+ */
+func factorable(_ n: Int) -> [Int] {
+    guard n > 0 else { return [] }
+    var factors: [Int] = []
+    for i in 1...n {
+        if n % i == 0 {
+            factors.append(i)
+        }
+    }
+    return factors
+}
